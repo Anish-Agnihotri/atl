9b3b3ab1cb7abbe9e429ab5fb11ebf4e65394e84
Anish Agnihotri <contact@anishagnihotri.com>
2019-08-28 19:20:46 -0400

    Fully completed menu

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,14 +1,70 @@
 import React from 'react';
 import logo from './logo.png';
 import { NavLink } from 'react-router-dom';
+import { HamburgerButton } from 'react-hamburger-button';
 import './index.css';
 
 class Header extends React.Component {
+
+	showMenu(event) {
+		event.preventDefault();
+		
+		this.setState({
+			showMenu: !this.state.showMenu,
+			open: !this.state.open
+		});
+	}
+
+	linkCloseMenu(event) {
+		event.preventDefault();
+
+		this.setState({
+			showMenu: false,
+			open: false
+		});
+	}
+
+	updateDimensions() {
+		if (window.innerWidth > 800) {
+			this.setState({
+				showMenu: false,
+				open: false,
+				showMenuIcon: false
+			});
+		}
+		else {
+			this.setState({
+				showMenuIcon: true
+			});
+		}
+	}
+	
+	constructor() {
+		super();
+		
+		this.state = {
+			showMenu: false,
+			showMenuIcon: true
+		};
+		
+		this.showMenu = this.showMenu.bind(this);
+		this.linkCloseMenu = this.linkCloseMenu.bind(this);
+	}
+
+	componentDidMount() {
+		this.updateDimensions();
+		window.addEventListener('resize', this.updateDimensions.bind(this));
+	}
+
+	componentWillUnmount() {
+		window.removeEventListener('resize', this.updateDimensions.bind(this));
+	}
+
 	render() {
 		return(
 			<div className="header">
 				<div className="subheader">
-					<span>Behind the scenes? Read the <a href='https://google.com' rel='noopener noreferrer' target='_blank'>documentation</a>.</span>
+					<span>Questions? Read the <a href='https://google.com' rel='noopener noreferrer' target='_blank'>documentation</a>.</span>
 				</div>
 				<div className="mainheader">
 					<div>
@@ -19,12 +75,41 @@ class Header extends React.Component {
 							<li><NavLink exact to="/" activeClassName="activeLink">Home</NavLink></li>
 							<li><NavLink to='/dashboard' activeClassName="activeLink">Dashboard</NavLink></li>
 							<li><NavLink to='/explore' activeClassName="activeLink">Explore</NavLink></li>
-							<li><a href='https://google.com'>Login via MetaMask</a></li>
+							<li><a className="loginMetaMask" href='https://google.com'>Login via MetaMask</a></li>
 						</ul>
+						{
+							this.state.showMenuIcon
+								? (
+									<HamburgerButton
+										open={this.state.open}
+										onClick={this.showMenu}
+										strokeWidth={3}
+										color="#000"
+										height={17}
+										width={25}
+									/>
+								)
+								: (
+									null
+								)
+						}
 					</div>
-					<div>
-
-					</div>
+					{
+						this.state.showMenu
+							? (
+								<div>
+									<ul>
+										<li><NavLink exact to="/" activeClassName="activeLink">Home</NavLink></li>
+										<li><NavLink to='/dashboard' activeClassName="activeLink">Dashboard</NavLink></li>
+										<li><NavLink to='/explore' activeClassName="activeLink">Explore</NavLink></li>
+										<li><a className="loginMetaMask" href='https://google.com'>Login via MetaMask</a></li>
+									</ul>
+								</div>
+							)
+							: (
+								null
+							)
+					}
 				</div>
 			</div>
 		);
