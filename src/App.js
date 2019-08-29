e11c68df20c0d01f4b3a04069ddb85d528a6a5b1
Anish Agnihotri <contact@anishagnihotri.com>
2019-08-28 20:56:52 -0400

    finished landing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import Web3 from 'Web3';
 import { BrowserRouter as Router, Route} from "react-router-dom";
 
 // Global header setup
 import Header from './components/header';
 
 // Page setup
-import Home from './pages/Home';
+import Home from './pages/Home/Home';
 import Dashboard from './pages/Dashboard';
 import Explore from './pages/Explore';
+
+// Web3 setup
 
 function App() {
   return (
